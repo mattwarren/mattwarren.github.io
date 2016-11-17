b08c83fa19bb57fc19829ee6364a9ad7518620da
Matt Warren <matt.warren@live.co.uk>
2016-11-17 12:37:32 +0000

    Added Normalized Stacked Bar Chart

--- datavis/dotnet-oss.js
+++ datavis/dotnet-oss.js
@@ -1,23 +1,39 @@
-// Initially empty, but gets populated via an Ajax request (below)
-sparklineData = {}
-dataIssues = {}
-dataPullRequests = {}
+// All this data is initially empty, but gets populated via an Ajax request (below)
+
+// The raw data, i.e. the json we got from the ajaz request
+dataIssues = {}, dataPullRequests = {};
+
+// The current sparkline data (can be 'Issues' or 'Pull Requests')
+sparklineData = {};
+
+//normalizedStackedIssueData = [], normalizedStackedPullRequestData = [];
 
 d3.json("/datavis/data-issues.json", function(error, json) {
   if (error) 
     return console.warn(error);
   dataIssues = json;
+
   sparklineData = dataIssues;
   updateSparklines();
   setSparklineHeaderText();
   setupButtonClickHandler();
+
+  var normalizedStackedIssueData = createNormalizedStackedBarChartData(json);
+  setupNormalizedStackedBarChart(normalizedStackedIssueData, ".g-chart-issues", "-issues");
 });
 
 d3.json("/datavis/data-pull-requests.json", function(error, json) {
   if (error) 
     return console.warn(error);
   dataPullRequests = json;
+
+  var normalizedStackedPullRequestData = createNormalizedStackedBarChartData(json);
+  setupNormalizedStackedBarChart(normalizedStackedPullRequestData, ".g-chart-pull-requests", "-pull-requests");
 });
+
+/***********************/
+/* Code for Sparklines */
+/***********************/
 
 function updateSparklines() {
   var dataTypes = Object.keys(sparklineData).sort(function(a, b) { 
@@ -87,6 +103,7 @@ function drawChart(chartId, url) {
         .attr('height', opts.height)
       .append('svg')
         .attr('id', opts.dom)
+        .attr('class', 'sparkline')
       .datum(sparklineData[opts.id].data)
       //.transition().duration(1000)
       .call(chart)
@@ -191,4 +208,162 @@ function setupButtonClickHandler() {
       }
     }
   });
+}
+
+/********************************************/
+/* Styles for Normalized Stacked Bar Charts */
+/********************************************/
+
+//Margin conventions
+//var margin = {top: 10, right: 50, bottom: 20, left: 227};
+var margin = {top: 10, right: 10, bottom: 20, left: 150};
+
+//var widther = window.outerWidth;
+var widther = 0;
+
+//var barHeight = 34;
+var barHeight = 25;
+
+// 4 bars, overall = 170, bar height = 33, gap = 9.5 (stride = 42.5)
+// 5 bars, overall = 205, bar height = 33, gap = 8   (stride = 41)
+
+var width = 0; // widther - margin.left - margin.right,
+    //height = 200 - margin.top - margin.bottom; // 4 bars
+    //height = 235 - margin.top - margin.bottom; // 5 bars
+    //height = 700 - margin.top - margin.bottom; // 16 bars
+    height = 550 - margin.top - margin.bottom; // 16 bars
+
+function createNormalizedStackedBarChartData(json) {
+  var dataKeys = Object.keys(json);
+  var tempData = [];
+  dataKeys.forEach(function(element) {
+    var itemData = json[element].data;
+    var communitySum = d3.sum(itemData, function(a) { return a.community });
+    var microsoftSum = d3.sum(itemData, function(a) { return a.microsoft });
+    var totalSum = d3.sum(itemData, function(a) { return a.total });
+    if (element !== "fake") {
+      //console.log(element + " - Sum: Community=" + communitySum + ", Microsoft=" + microsoftSum + ", Total=" + totalSum);
+      tempData.push({"category": element, "num": Math.round((microsoftSum / totalSum) * 100), "num2": 100});
+    }
+  });
+  return tempData;
+}
+
+function setupNormalizedStackedBarChart(data, className, classSuffix) {
+  widther = d3.select(className).node().clientWidth;
+  width = widther - margin.left - margin.right;
+
+  //Appends the svg to the chart-container div
+  var svg = d3.select(className).append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("class", "normalizedStackedBar")
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  var sortedCategories = data.sort(function(a, b) { 
+      return b.num - a.num; // ascending
+      //return a.num - b.num; // descending
+    }).map(function(obj){
+      return obj.category;
+    });
+
+  //Creates the xScale 
+  var xScale = d3.scale.linear()
+   .range([0, width]);
+
+  //Creates the yScale
+  var y0 = d3.scale.ordinal()
+    .rangeBands([height, 0], 0)
+    .domain(sortedCategories);
+
+  //Defines the y axis styles
+  var yAxis = d3.svg.axis()
+    .scale(y0)
+    .orient("left");
+
+  //Defines the y axis styles
+  var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom")
+    .tickFormat(function(d) {return d + "%"; })
+    .tickSize(height); 
+
+  //Draw the chart
+
+  //FORMAT data
+  data.forEach(function(d) {
+    d.num = +d.num;
+    d.num2 = +d.num2;
+  });
+
+  //Get the min/max value for the xScale
+  var maxX = d3.max(data, function(d) { return d.num2; });
+  var minX = d3.min(data, function(d) { return d.num; });
+
+  //Defines the xScale max
+  xScale.domain([0, maxX]);
+
+  //Appends the y axis
+  var yAxisGroup = svg.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  //Appends the x axis
+  var xAxisGroup = svg.append("g")
+    .attr("class", "x axis")
+    .call(xAxis); 
+
+  //Binds the data to the bars
+  var categoryGroup = svg.selectAll(".g-category-group")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "g-category-group")
+    .attr("transform", function(d) {
+      return "translate(0," + y0(d.category) + ")";
+    });
+
+  //Appends background bar
+  var bars2 = categoryGroup.append("rect")
+    .attr("width", function(d) { return xScale(d.num2); })
+    .attr("height", barHeight - 1 )
+    .attr("class", "g-num2" + classSuffix)
+    .attr("transform", "translate(0,4)");
+
+  //Appends main bar
+  var bars = categoryGroup.append("rect")
+    .attr("width", function(d) { return xScale(d.num); })
+    .attr("height", barHeight - 1 )
+    .attr("class", "g-num" + classSuffix)
+    .attr("transform", "translate(0,4)"); 
+
+  //Binds data to labels
+  var labelGroup = svg.selectAll("g-num")
+    .data(data)
+    .enter()
+    .append("g")
+    .attr("class", "g-label-group")
+    .attr("transform", function(d) {
+      return "translate(0," + y0(d.category) + ")";
+    });
+
+  //Appends main bar labels
+  var barLabels = labelGroup.append("text") 
+    .text(function(d) { return  d.num + "%"; })
+    .attr("x", function(d) { 
+      return xScale(d.num) - 37;})
+    .style("fill", function(d){
+      return "#696969";})
+    .attr("y", y0.rangeBand()/1.6 )
+    .attr("class", "g-labels")
+
+  var barLabelsOther = labelGroup.append("text") 
+    .text(function(d) { return (d.num2 - d.num) + "%"; })
+    .attr("x", function(d) { 
+      return xScale(d.num) + 6;})
+    .style("fill", function(d){
+      return "white";})
+    .attr("y", y0.rangeBand()/1.6 )
+    .attr("class", "g-labels") // + classSuffix);
 }
